cb21168fede5212ab79ee50e5879ad4b0ab3e264
vgdryukov <vgdryukov@gmail.com>
2025-11-09 20:47:42 +0400

    pre-release v1

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"internal/server"
+)
+
+func main() {
+	// Создаем логгер
+	logger := log.New(os.Stdout, "SERVER: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Создаем сервер
+	srv := server.NewServer(logger)
+
+	// Запускаем сервер
+	if err := srv.Start(); err != nil {
+		logger.Fatalf("Server failed to start: %v", err)
+	}
 }
